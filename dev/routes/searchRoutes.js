8e202bcd27deab4463c15919d5395f9acac8eb8e
mpolinowski <mpolinowski@gmail.com>
2017-02-12 22:15:15 +0800

    es5Data

--- dev/routes/searchRoutes.js
+++ dev/routes/searchRoutes.js
@@ -3,20 +3,21 @@ var searchRouter = express.Router();
 var elasticsearch = require('elasticsearch');
 
 var connectionString = 'localhost:9200';
+var _index = 'wiki2_en';
+var _type = 'article';
 
 var client = new elasticsearch.Client({
     host: connectionString,
     log: 'trace',
   });
 
-var _index = 'wiki_de';
-var _type = 'article';
 
 /* GET Search page. */
 searchRouter.route('/')
     .get(function(req, res) {
         res.render('Search', {
-            title: 'INSTAR Wiki Search',
+            title: 'INSTAR Wiki Suche',
+            breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search/">Suche</a></li>',
           });
       });
 
@@ -25,95 +26,33 @@ searchRouter.route('/')
 searchRouter.route('/Results')
     .get(function(req, res) {
 
-  var aggValue = req.query.agg_value;
-  var aggField = req.query.agg_field;
-
-  var filter = {};
-  filter[aggField] = aggValue;
-
-  client.search({
-      index: _index,
-      type: _type,
-      body: {
-          query: {
-              filtered: {
+            client.search({
+                index: _index,
+                type: _type,
+                body: {
                   query: {
                       multi_match: {
                           query: req.query.q,
-                          fields: ['title^100', 'tags^50', 'abstract^20', 'description^10', 'description_de^10', 'models^5', 'chapter^5', 'title2^5'],
+                          fields: ['title^100', 'tags^50', 'abstract^20', 'description^10', 'models^5', 'chapter^5', 'title2^5'],
                           fuzziness: 1,
                         },
                     },
-                  filter: {
-                      term: (aggField ? filter : undefined),
-                    },
                 },
-
-            },
-          aggs: {
-              title: {
-                  terms: {
-                      field: 'title.raw',
-                    },
-                },
-              tags: {
-                  terms: {
-                      field: 'tags.raw',
-                    },
-                },
-              abstract: {
-                  terms: {
-                      field: 'abstract.raw',
-                    },
-                },
-              title2: {
-                  terms: {
-                      field: 'title2.raw',
-                    },
-                },
-              chapter: {
-                  terms: {
-                      field: 'chapter.raw',
-                    },
-                },
-            },
-          suggest: {
-              text: req.query.q,
-              simple_phrase: {
-                  phrase: {
-                      field: 'title',
-                      size: 1,
-                      real_word_error_likelihood: 0.95,
-                      max_errors: 0.5,
-                      gram_size: 2,
-                      direct_generator: [{
-                          field: 'title',
-                          suggest_mode: 'always',
-                          min_word_length: 1,
-                        },],
-                      highlight: {
-                          pre_tag: '<b><em>',
-                          post_tag: '</em></b>',
-                        },
-                    },
-                },
-            },
-        },
-    }).then(function(resp) {
-      res.render('Search_Results', {
-          title: 'INSTAR Wiki Search Results',
-          response: resp,
-          query: req.query.q,
-          breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search_Results/">Suchergebnisse</a></li>',
-        });
-    }, function(err) {
-      console.trace(err.message);
-      res.render('Search_Results', {
-          title: 'INSTAR Wiki Search Results',
-          response: err.message,
-          breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search_Results/">Suchergebnisse</a></li>',
-        });
-    });
-});
+              }).then(function(resp) {
+                res.render('Search_Results', {
+                    title: 'INSTAR Wiki Suchergebnisse',
+                    response: resp,
+                    query: req.query.q,
+                    breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search/">Suche</a></li><li class="breadcrumb-item"><a href="/Search/Results/">Suchergebnisse</a></li>',
+                  });
+              }, function(err) {
+                console.trace(err.message);
+                res.render('Search_Results', {
+                    title: 'INSTAR Wiki Suchergebnisse',
+                    response: err.message,
+                    breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search/">Suche</a></li><li class="breadcrumb-item"><a href="/Search/Results/">Suchergebnisse</a></li>',
+                  });
+              });
+          });
 
 module.exports = searchRouter;
