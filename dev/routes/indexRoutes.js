78638a0ed948c3ac020680e5d2f62c26f9ad7bab
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-08-07 18:32:42 +0800

    description added

--- dev/routes/indexRoutes.js
+++ dev/routes/indexRoutes.js
@@ -5,11 +5,11 @@ var indexRouter = express.Router();
 indexRouter.route('/')
     .get(function(req, res) {
         res.render('Index', {
-            title: 'INSTAR Wiki',
+            title: 'INSTAR Wiki - IP Kamera und Computer Netzwerk Überwachungskameras, IP-Kameras, Netzwerkkameras, Überwachungskameras , IP-Cam, Nachtsichtkameras , spycams, ipcam, Cloud-Aufnahme, Video-Überwachung, NVR, Netzwerk-Videorecorder, Sicherheitstechnik Wissensdatenbank.',
             ampimg: '/images/Search/QI_SearchThumb_SetupWizard.jpg',
             headline: 'INSTAR IP Camera and Network Technology Knowledgebase',
             canonical: '/',
-            description: 'INSTAR Wiki - IP Kamera Wissensdatenbank',
+            description: 'INSTAR ist eine der bekanntesten Marken für hochwertige Sicherheitstechnik die in Deutschland entwickelt wird. INSTAR bietet IP Kameras für den Innen und Außenbereich, WLAN Netzwerkkameras, HD IP Kameras, steuerbare IP Kameras, IP Kameras mit Infrarot Nachtsicht sowie PoE Injektoren, Gigabit PoE Injectoren, Powerline Adapter, Router, Switches, Bewegungsmelder mit PIR und Mikrowellen, Mikrofone, Objektive, eine patentierte Heizung sowie weiteres Zubehör für IP Überwachungskameras.',
           });
       });
 
