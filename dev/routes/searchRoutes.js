250c1993cc30bacdcc01522f7482253f2e1135fd
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-08-28 20:04:14 +0800

    eng index

--- dev/routes/searchRoutes.js
+++ dev/routes/searchRoutes.js
@@ -33,7 +33,7 @@ searchRouter.route('/Results')
                   query: {
                       multi_match: {
                           query: req.query.q,
-                          fields: ['title^100', 'tags^50', 'abstract^20', 'description^10', 'models^5', 'chapter^5', 'title2^5'],
+                          fields: ['title^100', 'tags^50', 'abstract^20', 'description^10', 'models^5', 'chapter^5'],
                           fuzziness: 1,
                         },
                     },
@@ -57,6 +57,40 @@ searchRouter.route('/Results')
 
 module.exports = searchRouter;
 
+// Search
+// searchRouter.route('/Results')
+//     .get(function(req, res) {
+//
+//             client.search({
+//                 index: _index,
+//                 type: _type,
+//                 body: {
+//                   query: {
+//                       multi_match: {
+//                           query: req.query.q,
+//                           fields: ['title^100', 'tags^50', 'abstract^20', 'description^10', 'models^5', 'chapter^5', 'title2^5'],
+//                           fuzziness: 1,
+//                         },
+//                     },
+//                 },
+//               }).then(function(resp) {
+//                 res.render('Search_Results', {
+//                     title: 'INSTAR Wiki Search Results',
+//                     response: resp,
+//                     query: req.query.q,
+//                     breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search/">Suche</a></li><li class="breadcrumb-item"><a href="/Search/Results/">Suchergebnisse</a></li>',
+//                   });
+//               }, function(err) {
+//                 console.trace(err.message);
+//                 res.render('Search_Results', {
+//                     title: 'INSTAR Wiki Search Results',
+//                     response: err.message,
+//                     breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Search/">Suche</a></li><li class="breadcrumb-item"><a href="/Search/Results/">Suchergebnisse</a></li>',
+//                   });
+//               });
+//           });
+
+// #############
 
 // // Search
 // searchRouter.route('/Results')
