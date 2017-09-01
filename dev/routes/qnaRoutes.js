5b715be62ec7b9529bdfac4a9fd64138a25c8d00
Mike Polinowski <mpolinowski@users.noreply.github.com>
2017-09-01 20:23:28 +0800

    QnA HEAD update

--- dev/routes/qnaRoutes.js
+++ dev/routes/qnaRoutes.js
@@ -5,10 +5,11 @@ qnaRouter.route('/')
   .get(function(req, res) {
       res.render('./QnA/QnA_Tiddler', {
           title: 'Fragen & Antworten — Häufig gestellte Kundenanfragen und Support Antworten',
-          ampimg: '/images/Search/P_SearchThumb_Products.jpg',
-          headline: 'Fragen & Antworten',
+          ampimg: '/images/Search/FAQ_SearchThumb.jpg',
+          headline: 'Kundenanfrage über INSTAR Überwachungskameras, Kamera Software oder Handy Apps und Netzwerktechnik',
           breadcrumbs: '<li class="breadcrumb-item"><a href="/">Home</a></li><li class="breadcrumb-item"><a href="/Fragen_und_Antworten/">Fragen & Antworten</a></li>',
           canonical: '/Fragen_und_Antworten/',
+          description: 'Kundenanfrage über INSTAR Überwachungskameras, Kamera Software oder Handy Apps und Netzwerktechnik',
         });
     });
 
